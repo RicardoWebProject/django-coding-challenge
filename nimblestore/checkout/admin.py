35ff2485628816784dbb976fac48b6434797e86c
RicardoWebProject <luisrijhn09@gmail.com>
2026-03-05 12:36:34 -0600

    Added: Product and Admin registration

--- nimblestore/checkout/admin.py
+++ nimblestore/checkout/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Order, OrderItem, Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "price", "stock_quantity"]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ["unit_price"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "status", "created_at", "updated_at"]
+    list_filter = ["status"]
+    inlines = [OrderItemInline]
 
 
 admin.site.register(Product, ProductAdmin)
+
